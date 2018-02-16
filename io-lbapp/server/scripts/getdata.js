3a56fcf983f6041dbe6d138b9891ed027eb6cdb7
Julien Ledun <j.ledun@iosystems.fr>
2018-02-16 17:01:59 +0100

    reduce data size
    Signed-off-by: Julien Ledun <j.ledun@iosystems.fr>

--- io-lbapp/server/scripts/getdata.js
+++ io-lbapp/server/scripts/getdata.js
@@ -4,7 +4,7 @@ const app = require('../server');
 const sql = app.datasources.PREMIX;
 const fs = require('fs');
 
-sql.connector.query("SELECT * FROM `LOG_REFROIDISSEUR` where TIMESTAMP >= '2017-06-01 00:00:00' AND TIMESTAMP <= '2017-06-20 00:00:00'", (err, data) => {
+sql.connector.query("SELECT * FROM `EVENTHISTORY` where Ev_Time >= '2018-01-01 00:00:00';", (err, data) => {
   if (err) throw err;
-  fs.writeFileSync('/tmp/processdatas.json', JSON.stringify(data));
+  fs.writeFileSync('/tmp/eventhistory.json', JSON.stringify(data));
 });
