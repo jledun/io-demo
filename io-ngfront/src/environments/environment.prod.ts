3554eb9a8398d527591e130719ab2334bb30db1f
Julien Ledun <j.ledun@iosystems.fr>
2018-02-20 18:18:01 +0100

    switch to aws ec2 East US
    Signed-off-by: Julien Ledun <j.ledun@iosystems.fr>

--- io-ngfront/src/environments/environment.prod.ts
+++ io-ngfront/src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   name: "prod",
   lbApp: {
-    ip: "ec2-52-47-151-127.eu-west-3.compute.amazonaws.com:3000",
+    // ip: "ec2-52-47-151-127.eu-west-3.compute.amazonaws.com:3000",  // aws ec2 France
+    ip: "http://ec2-18-219-235-189.us-east-2.compute.amazonaws.com:3000/",  // aws ec2 East US
     api: 'api'
   }
 };
