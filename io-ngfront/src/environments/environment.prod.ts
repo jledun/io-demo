94faea35505251dbbdfec3a97ac6238ad89758b1
Julien Ledun <j.ledun@iosystems.fr>
2018-02-20 18:27:33 +0100

    switch to port 80
    Signed-off-by: Julien Ledun <j.ledun@iosystems.fr>

--- io-ngfront/src/environments/environment.prod.ts
+++ io-ngfront/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   name: "prod",
   lbApp: {
     // ip: "ec2-52-47-151-127.eu-west-3.compute.amazonaws.com:3000",  // aws ec2 France
-    ip: "http://ec2-18-219-235-189.us-east-2.compute.amazonaws.com:3000/",  // aws ec2 East US
+    ip: "http://ec2-18-219-235-189.us-east-2.compute.amazonaws.com",  // aws ec2 East US
     api: 'api'
   }
 };
